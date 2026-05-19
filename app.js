af8289eed723d4ee6a3e3ba4a1d0bd35e139d655
Sai Rohitth <sairohitth321@gmail.com>
2026-05-19 01:35:14 -0400

    convert session auth to passport authentication

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import session from 'express-session'
-import bcrypt from 'bcrypt'
 import {User} from './models/User.js'
+import passport from 'passport'
+import LocalStrategy from 'passport-local'
 
 const app = express()
 
@@ -19,6 +20,12 @@ app.use(session({
     resave:false,
     saveUninitialized:false
 }))
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 app.get('/',(req,res)=>{
     res.render('home')
@@ -28,40 +35,32 @@ app.get('/login',(req,res)=>{
     res.render('login')
 })
 
-app.post('/login',async(req,res)=>{
-    const {username,password}=req.body
-    const user=await User.findOne({username:username})
-    if(!user){
-        return res.send('Invalid username')
+app.post('/login',
+    passport.authenticate('local',{
+        failureRedirect:'/login'
+    }),
+    (req,res)=>{
+        res.redirect('/profile')
     }
-    const validPassword=await bcrypt.compare(password,user.password)
-    if(!validPassword){
-        return res.send('Invalid password')
-    }
-    req.session.user_id=user._id
-    // console.log("login",req.session)
-    res.redirect('/profile')
-})
+)
 
 const isLoggedIn=(req,res,next)=>{
-    if(!req.session.user_id){
+    if(!req.isAuthenticated()){
         return res.redirect('/login')
     }
     next()
 }
 
-app.get('/profile',isLoggedIn,async(req,res)=>{
-    // console.log("profile",req.session)
-    const user=await User.findById(req.session.user_id)
-    res.render('profile',{username:user.username})
+app.get('/profile',isLoggedIn,(req,res)=>{
+    console.log(req.user)
+    res.render('profile',{username:req.user.username})
 })
 
-app.post('/logout',(req,res)=>{
-    // console.log("before destroying")
-    // console.log(req.session)
-    req.session.destroy(()=>{
-        // console.log("session destroyed")
-        // console.log("session :",req.session)
+app.post('/logout',(req,res,next)=>{
+    req.logout((err)=>{
+        if(err){
+            return next(err)
+        }
         res.redirect('/login')
     })
 })
@@ -72,11 +71,14 @@ app.get('/register',(req,res)=>{
 
 app.post('/register',async(req,res)=>{
     const {username,password}=req.body
-    const hashed=await bcrypt.hash(password,12)
-    const user=new User({username,password:hashed})
-    await user.save()
-    req.session.user_id=user._id
-    res.redirect('/profile')
+    const user=new User({username})
+    const registeredUser=await User.register(user,password)
+    req.login(registeredUser,(err)=>{
+        if(err){
+            return res.send(err)
+        }
+        res.redirect('/profile')
+    })
 })
 
 app.listen(3000,()=>{
--- models/User.js
+++ models/User.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose'
+import passportLocalMongoose from 'passport-local-mongoose'
 
 const userSchema=new mongoose.Schema({
-    username:String,
-    password:String
+    
 })
+
+userSchema.plugin(passportLocalMongoose.default)
 
 export const User=mongoose.model('User',userSchema)
